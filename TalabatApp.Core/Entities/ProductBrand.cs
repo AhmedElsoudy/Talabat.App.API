95adfee516968fac391e8b9f4d2a05c6fb66f422
AhmedElsoudy <ajmd9563@gmail.com>
2024-12-12 21:49:49 +0200

    Add project files.

---
+++ TalabatApp.Core/Entities/ProductBrand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalabatApp.Core.Entities
+{
+    public class ProductBrand : BaseEntity
+    {
+        public string Name { get; set; }
+
+        //public IEnumerable<Product> Products { get; set; } = new HashSet<Product>();
+
+
+
+    }
+}
---
+++ TalabatApp.Core/Entities/ProductCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalabatApp.Core.Entities
+{
+    public class ProductCategory : BaseEntity
+    {
+        public string Name { get; set; }
+
+        //public IEnumerable<Product> MyProperty { get; set; } = new HashSet<Product>();
+
+    }
+}
