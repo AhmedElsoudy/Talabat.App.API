e79ee4b57624304384b8baf924f9741d4047e37a
AhmedElsoudy <ajmd9563@gmail.com>
2024-12-21 19:43:37 +0200

    ErrorHandlingPart1

---
+++ TalabatApp/Controllers/BuggyController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TalabatApp.Repository.Data;
+
+namespace TalabatApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuggyController : ControllerBase
+    {
+        private readonly StoreContext _dbcontext;
+
+        public BuggyController(StoreContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+
+        [HttpGet("notfound")]                // GET : api/Buggy/notfound
+        public ActionResult GetNotFound()    
+        {
+            var product = _dbcontext.Products.Find(100);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpGet("servererror")]            // GET : api/Buggy/servererror
+        public ActionResult GetServerError()
+        {
+            var product = _dbcontext.Products.Find(100);
+            var productDto = product.ToString();
+            return Ok(productDto);
+
+        }
+
+        [HttpGet("badrequest")]            // GET : api/Buggy/badrequest
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest();
+        }
+
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetBadRequest(int id)
+        {
+            return Ok();
+        }
+
+
+    }
+}
--- TalabatApp/Helpers/ProductPictureUrlResolver.cs
+++ TalabatApp/Helpers/ProductPictureUrlResolver.cs
@@ -21,5 +21,7 @@ namespace TalabatApp.Helpers
 
             return string.Empty;
         }
+
+
     }
 }
