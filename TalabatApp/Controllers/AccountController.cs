341c893d087d995f02ffb8986eaec5fb41c60b4c
AhmedElsoudy <ajmd9563@gmail.com>
2025-01-12 20:14:26 +0200

    Register EndPoint

--- TalabatApp/Controllers/AccountController.cs
+++ TalabatApp/Controllers/AccountController.cs
@@ -21,13 +21,13 @@ namespace TalabatApp.Controllers
 
         [HttpPost("login")]  // POST : /api/Account/login
 
-        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        public async Task<ActionResult<UserDto>> Login(LoginDto Form)
         {
-            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            var user = await _userManager.FindByEmailAsync(Form.Email);
             if (user is null) return Unauthorized(new ApiErrorResponse(401));
 
-            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
-            if (result.Succeeded is false) return Unauthorized(new ApiErrorResponse(401));
+            var result = await _signInManager.CheckPasswordSignInAsync(user, Form.Password, false);
+            if (result.Succeeded is false) return Unauthorized(new ApiErrorResponse(StatusCodes.Status401Unauthorized));
             return Ok(new UserDto()
             {
                 DisplayName = user.DisplayName,
@@ -35,7 +35,37 @@ namespace TalabatApp.Controllers
                 Token = "this Will be Token"
             });
 
-        } 
+        }
+
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto Form)
+        {
+            // Create User
+            var user = new AppUser()
+            {
+                DisplayName = Form.DisplayName,
+                Email = Form.Email,
+                UserName = Form.Email.Split("@")[0],
+                PhoneNumber = Form.PhoneNumber
+            };
+
+            // Add User To Database
+
+            var Result = await _userManager.CreateAsync(user, Form.Password);
+
+            // return Data 
+
+            if (Result.Succeeded is false) return BadRequest(new ApiErrorResponse(StatusCodes.Status400BadRequest));
+
+            return Ok(new UserDto()
+            {
+                DisplayName = user.DisplayName,
+                Email = user.Email,
+                Token = "this will be token"
+            });
+
+        }
 
  
     }
