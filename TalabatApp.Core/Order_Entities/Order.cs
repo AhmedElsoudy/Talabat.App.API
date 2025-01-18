53738bea6f8808f17806e2dd79e11ab2a7cf11d0
AhmedElsoudy <ajmd9563@gmail.com>
2025-01-18 22:51:13 +0200

    Order Entities

---
+++ TalabatApp.Core/Order_Entities/Order.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalabatApp.Core.Entities;
+
+namespace TalabatApp.Core.Order_Entities
+{
+    public class Order : BaseEntity
+    {
+        public Order()
+        {
+            
+        }
+        public Order(string buyerEmail, OrderStatus status, ShippingAddress shippingAddress, DeliveryMethod deliveryMethod, ICollection<OrderItem> items, decimal subTotal)
+        {
+            BuyerEmail = buyerEmail;
+            Status = status;
+            ShippingAddress = shippingAddress;
+            DeliveryMethod = deliveryMethod;
+            Items = items;
+            SubTotal = subTotal;
+        }
+
+        public string BuyerEmail { get; set; }
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.UtcNow;
+        public OrderStatus Status { get; set; }
+        public ShippingAddress ShippingAddress { get; set; }
+        public DeliveryMethod DeliveryMethod { get; set; }
+        public ICollection<OrderItem> Items { get; set; } = new HashSet<OrderItem>();
+        public decimal SubTotal { get; set; }
+        public decimal GetTotal() => SubTotal + DeliveryMethod.Cost;
+        public string PaymentIntentId { get; set; }
+
+    }
+}
