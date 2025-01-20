455501ab99914f54940552c528d51606483d2547
AhmedElsoudy <ajmd9563@gmail.com>
2025-01-20 16:32:49 +0200

    Order Configurations

---
+++ TalabatApp.Repository/Data/Configurations/OrderConfigurations.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalabatApp.Core.Order_Entities;
+
+namespace TalabatApp.Repository.Data.Configurations
+{
+    public class OrderConfigurations : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+
+            builder.Property(O => O.Status)
+                   .HasConversion(
+                        InputConversion => InputConversion.ToString(),
+                        InputConversion => (OrderStatus) Enum.Parse(typeof(OrderStatus), InputConversion)
+                   );
+
+            builder.OwnsOne(Order => Order.ShippingAddress, ShippingAddress => ShippingAddress.WithOwner());
+
+            builder.Property(Order => Order.SubTotal)
+                   .HasColumnType("decimal(18,2)");
+
+
+        }
+    }
+}
