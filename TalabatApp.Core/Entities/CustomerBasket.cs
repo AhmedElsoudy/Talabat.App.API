2f8b09e431ed4601b02fe27103ec921fe3a6f487
AhmedElsoudy <ajmd9563@gmail.com>
2025-02-25 00:28:10 +0200

    CreateOrUpdatePaymentIntent [Function]

--- TalabatApp.Core/Entities/CustomerBasket.cs
+++ TalabatApp.Core/Entities/CustomerBasket.cs
@@ -17,6 +17,10 @@ namespace TalabatApp.Core.Entities
 
         public string Id { get; set; }
         public List<BasketItem> Items { get; set; }
+        public string PaymentIntentId { get; set; }
+        public string ClientSecret { get; set; }
+        public int? DeliveryMethodId { get; set; }
+
 
 
     }
