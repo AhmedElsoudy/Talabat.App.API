4ad414e5eb3e7c00a15756190e92836e6514866c
AhmedElsoudy <ajmd9563@gmail.com>
2025-02-27 23:17:58 +0200

    Caching Service

---
+++ TalabatApp.Core/Services.Contract/IResponseCacheService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalabatApp.Core.Services.Contract
+{
+    public interface IResponseCacheService
+    {
+        // Cache Data
+
+        Task CacheResponseAsync(string CacheKey, object Response, TimeSpan ExpireTime);
+
+
+
+        // Get Cached Data
+        Task<string?> GetCachedResponse(string CacheKey);
+
+
+
+    }
+}
---
+++ TalabatApp.Services/ResponseCacheService.cs
@@ -0,0 +1,43 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TalabatApp.Core.Services.Contract;
+
+namespace TalabatApp.Services
+{
+    public class ResponseCacheService : IResponseCacheService
+    {
+        private readonly IDatabase _database;
+        public ResponseCacheService(IConnectionMultiplexer Redis)
+        {
+            _database = Redis.GetDatabase();
+        }
+
+        // Caching Data
+        public async Task CacheResponseAsync(string CacheKey, object Response, TimeSpan ExpireTime)
+        {
+            if (Response is null) return;
+            var options = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            var serializedResponse = JsonSerializer.Serialize(Response, options);
+           await  _database.StringSetAsync(CacheKey, serializedResponse, ExpireTime);
+
+        }
+
+        // Get Cached Data
+
+        public async Task<string?> GetCachedResponse(string CacheKey)
+        {
+            var CachedResponse =  await _database.StringGetAsync(CacheKey);
+            if (CachedResponse.IsNullOrEmpty) return null;
+            return CachedResponse;
+
+        }
+    }
+}
